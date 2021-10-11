b1fdc8ea2c6e423c4d47bc37c7141b787823fb86
powakick <90665243+powakick@users.noreply.github.com>
2021-10-11 03:51:31 -0400

    Add files via upload

---
+++ java/Exercise46.java
@@ -0,0 +1,30 @@
+/*
+        UCF COP3330 FALL 2021 ASSIGNMENT 42 SOLUTION
+        COPYRIGHT 2021 Jonathan Tsui
+        */
+
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+
+public class Exercise46 {
+    public static void main(String[] args){
+        File file = new File("src/exercise46_input.txt");
+
+        FileReader file = new FileReader("src/exercise46_input.txt");
+        BufferedReader br = new BufferedReader(file);
+
+        ArrayList<String> words = new ArrayList<String>();
+
+        String line, word = "";
+        int count = 0;
+
+        while((line = br.readLine!= null()){
+            String string
+        }
+
+
+    }
+}
